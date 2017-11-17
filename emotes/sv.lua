39588922bacfa3b3718f2da606b5caf1c90aa2e6
Jacob <16978528+blockba5her@users.noreply.github.com>
2017-11-16 19:02:50 -0800

    Fixed command invoking

--- emotes/sv.lua
+++ emotes/sv.lua
@@ -1,13 +1,26 @@
-RegisterCommand("emote", function(source, args, rawCommand)
-    if args[1] ~= nil then
-        TriggerClientEvent("emote:invoke", source, args[1])
-    else
-        TriggerClientEvent("chatMessage", source, "ERROR", {255,0,0}, "^7Use \"^3/emotes^7\" to display all of the emotes")
+AddEventHandler('chatMessage', function(source, n, msg)
+    local args = stringsplit(msg, ' ')
+        
+    if string.lower(args[1]) == "/emotes" then
+        TriggerClientEvent("emote:display", source)
+    elseif string.lower(args[1]) == "/emote" then
+        if args[2] ~= nil then
+            TriggerClientEvent("emote:invoke", source, args[2])
+        else
+            TriggerClientEvent("chatMessage", source, "ERROR", {255,0,0}, "^7Use \"^3/emotes^7\" to display all of the emotes")
+        end
+    elseif string.lower(args[1]) == "/cancelemote" then
+        TriggerClientEvent("emote:cancelnow", source)
     end
-end, false)
-RegisterCommand("emotes", function(source, args, rawCommand)
-    TriggerClientEvent("emote:display", source)
 end)
-RegisterCommand("cancelemote", function(source, args, rawCommand)
-    TriggerClientEvent("emote:cancelnow", source)
-end)
+function stringsplit(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t={} ; i=1
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
+end
